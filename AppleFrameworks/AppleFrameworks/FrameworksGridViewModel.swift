1f18b7a784def7162d6458c33493457b8fad7eb8
nikitakaralyus <nikitakaralyus@gmail.com>
2022-10-09 20:00:18 +0300

    Created FrameworksGridViewModel

---
+++ AppleFrameworks/AppleFrameworks/FrameworksGridViewModel.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+final class FrameworksGridViewModel: ObservableObject {
+    var selectedFramework: Framework? {
+        didSet {
+            isShowingDetailView = true
+        }
+    }
+    
+    @Published var isShowingDetailView = false
+}

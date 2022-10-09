d3b241e7523eb352d21ee5bda3bdea8fd153bc4d
nikitakaralyus <nikitakaralyus@gmail.com>
2022-10-09 18:20:11 +0300

    Replaced ContentView with FrameworksGridView

--- AppleFrameworks/AppleFrameworks/AppleFrameworksApp.swift
+++ AppleFrameworks/AppleFrameworks/AppleFrameworksApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct AppleFrameworksApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FrameworksGridView()
         }
     }
 }
---
+++ AppleFrameworks/AppleFrameworks/FrameworksGridView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct FrameworksGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworksGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworksGridView()
+    }
+}

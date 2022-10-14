9ce2217263b4db760a5d5ca2eb2ada7c8906470c
nikitakaralyus <nikitakaralyus@gmail.com>
2022-10-14 18:40:10 +0300

    Scaffold project up

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Nikita Karalius on 14.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetizersTabView()
+        }
+    }
+}

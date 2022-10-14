c40250ebc115559cac760932103a1d3d1be428f5
nikitakaralyus <nikitakaralyus@gmail.com>
2022-10-14 21:10:35 +0300

    Created Appetizer model

---
+++ Appetizers/Appetizers/Model/Appetizer.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}

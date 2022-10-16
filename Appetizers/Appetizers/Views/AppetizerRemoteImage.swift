e2133ee6744c9921b16a5e7260961b58138d3b60
nikitakaralyus <nikitakaralyus@gmail.com>
2022-10-16 11:35:15 +0300

    Created AppetizerRemoteImage

---
+++ Appetizers/Appetizers/Views/AppetizerRemoteImage.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+final class ImageLoader: ObservableObject {
+    @Published var loadedImage: Image?
+    
+    func load(from url: String, using service: AppetizerService = .shared) {
+        service.loadImage(from: url) { uiImage in
+            guard let uiImage = uiImage else { return }
+            DispatchQueue.main.async {
+                self.loadedImage = Image(uiImage: uiImage)
+            }
+        }
+    }
+}
+
+struct ImageWithPlaceholder: View {
+    var targetImage: Image?
+    var placeholder: Image
+    
+    var body: some View {
+        targetImage?.resizable() ??
+        placeholder.resizable()
+    }
+}
+
+struct AppetizerRemoteImage: View {
+    @StateObject var imageLoader = ImageLoader()
+    @State var imageURL: String
+    
+    var body: some View {
+        ImageWithPlaceholder(
+            targetImage: imageLoader.loadedImage,
+            placeholder: Image("food-placeholder"))
+    }
+}

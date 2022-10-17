fc85251b24c043dd1d7ee2902f7984482e565b49
nikitakaralyus <nikitakaralyus@gmail.com>
2022-10-17 16:04:18 +0300

    Added height and width parameters

---
+++ Appetizers/Appetizers/Screens/Order/OrderListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OrderListViewModel.swift
+//  Appetizers
+//
+//  Created by Nikita Karalius on 17.10.2022.
+//
+
+import Foundation
--- Appetizers/Appetizers/Views/Buttons/AppetizerButtonLabel.swift
+++ Appetizers/Appetizers/Views/Buttons/AppetizerButtonLabel.swift
@@ -2,16 +2,20 @@ import SwiftUI
 
 struct AppetizerButtonLabel: View {
     private let text: String
+    var width: CGFloat
+    var height: CGFloat
     
-    init(_ text: String) {
+    init(_ text: String, width: CGFloat = 260, height: CGFloat = 50) {
         self.text = text
+        self.width = width
+        self.height = height
     }
     
     var body: some View {
         Text(text)
             .font(.title3)
             .fontWeight(.semibold)
-            .frame(width: 260, height: 50)
+            .frame(width: width, height: height)
             .foregroundColor(.white)
             .background(Color.brandPrimary)
             .cornerRadius(8)

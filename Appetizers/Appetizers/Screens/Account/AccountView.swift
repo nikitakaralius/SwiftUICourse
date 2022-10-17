b10e42905327c34b3b333aef7ddd28b1a6ba62db
nikitakaralyus <nikitakaralyus@gmail.com>
2022-10-17 13:51:17 +0300

    Created AccountView

--- Appetizers/Appetizers/Screens/Account/AccountView.swift
+++ Appetizers/Appetizers/Screens/Account/AccountView.swift
@@ -1,10 +1,32 @@
 import SwiftUI
 
 struct AccountView: View {
+    @StateObject private var viewModel = AccountViewModel()
+    
     var body: some View {
         NavigationView {
-            Text("Account View")
-                .navigationTitle("🧑🏼 Account")
+            Form {
+                Section("Personal Information") {
+                    TextField("First Name", text: $viewModel.firstName)
+                    TextField("Last Name", text: $viewModel.lastName)
+                    TextField("Email", text: $viewModel.email)
+                        .keyboardType(.emailAddress)
+                        .autocorrectionDisabled()
+                    DatePicker("Birthdate", selection: $viewModel.birthdate, displayedComponents: .date)
+                    Button("Save Changes") {
+                        
+                    }
+                }
+                
+                Section("Requests") {
+                    Toggle("Extra Napkins",
+                           isOn: $viewModel.areExtraNapkinsOn)
+                    Toggle("Frequent Refills",
+                           isOn: $viewModel.areFrequentRefillsOn)
+                }
+                .tint(.brandPrimary)
+            }
+            .navigationTitle("🧑🏼 Account")
         }
     }
 }

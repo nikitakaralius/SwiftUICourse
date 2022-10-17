4b16c4f867b9d30c4e398e725b49df1092837c50
nikitakaralyus <nikitakaralyus@gmail.com>
2022-10-17 16:04:55 +0300

    Created OrderListViewModel

--- Appetizers/Appetizers/Screens/Order/OrderListViewModel.swift
+++ Appetizers/Appetizers/Screens/Order/OrderListViewModel.swift
@@ -1,8 +1,19 @@
-//
-//  OrderListViewModel.swift
-//  Appetizers
-//
-//  Created by Nikita Karalius on 17.10.2022.
-//
+import SwiftUI
 
-import Foundation
+protocol OrderList {
+    func add(appetizer: Appetizer)
+}
+
+final class OrderListViewModel: ObservableObject, OrderList {
+    @Published var appetizers: [Appetizer] = []
+    
+    var summary: Double {
+        appetizers.reduce(0) { partialResult, appetizer in
+            partialResult + appetizer.price
+        }
+    }
+    
+    func add(appetizer: Appetizer) {
+        appetizers.append(appetizer)
+    }
+}

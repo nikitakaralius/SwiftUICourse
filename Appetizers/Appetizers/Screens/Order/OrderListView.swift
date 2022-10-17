7f030b74e0810dfdc6edd8f41e686c371098cd37
nikitakaralyus <nikitakaralyus@gmail.com>
2022-10-17 16:05:10 +0300

    Created OrderListView

--- Appetizers/Appetizers/Screens/Order/OrderListView.swift
+++ Appetizers/Appetizers/Screens/Order/OrderListView.swift
@@ -1,16 +1,42 @@
 import SwiftUI
 
 struct OrderListView: View {
+    @ObservedObject var viewModel: OrderListViewModel
+    
     var body: some View {
         NavigationView {
-            Text("Order View")
-                .navigationTitle("💶 Orders")
+            VStack {
+                List(viewModel.appetizers, rowContent: { appetizer in
+                    AppetizerCellView(appetizer: appetizer)
+                })
+                .listStyle(.plain)
+
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    AppetizerButtonLabel(
+                        "$\(viewModel.summary.formatted()) - Place to Order",
+                        width: 320,
+                        height: 50)
+                    .padding()
+                }
+            }
+            .navigationTitle("💶 Orders")
         }
+        .navigationViewStyle(.stack)
     }
 }
 
 struct OrderListView_Previews: PreviewProvider {
     static var previews: some View {
-        OrderListView()
+        OrderListView(viewModel: viewModel)
+    }
+    
+    static var viewModel: OrderListViewModel {
+        let viewModel = OrderListViewModel()
+        viewModel.appetizers = MockData.appetizers + MockData.appetizers
+        return viewModel
     }
 }
